064277ae88c4df019f27e97031a35dcd3cd0ee93
Rika Petersen <rikapetersen@web.de>
2019-01-26 21:11:01 +0100

    Rotation Final

--- Assets/Scripts/RotateWorld.cs
+++ Assets/Scripts/RotateWorld.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class RotateWorld : MonoBehaviour
 {
-
-
     public float horizontalSpeed = 20.0F;
     public float verticalSpeed = 20.0F;
+    bool isRotating;
+    RaycastHit hit;
+    Ray ray;
+
+    private Camera cam;
+    GameObject planet;
+
+    void Start()
+    {
+        planet = GameObject.FindWithTag("Planet");
+        cam = Camera.main;
+        isRotating = false;
+    }
 
     void Update()
     {
         float h = horizontalSpeed * Input.GetAxis("Mouse X");
         float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        Debug.Log(h);
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(1))
         {
-            transform.Rotate(v, h, 0);
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (!isRotating)
+            {
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    isRotating = true;
+                    transform.Rotate(new Vector3(v, -h, 0), Space.World);
+
+                }
+            }
+            else
+            {
+                transform.Rotate(new Vector3(v, -h, 0), Space.World);
+            }
         }
+        if (Input.GetMouseButtonUp(1))
+        {
+            isRotating = false;
+
+        }
+
+
     }
 }
- 
