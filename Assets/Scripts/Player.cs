cf8043a415ecfd56044fd0bf7b9565d39d903691
Block <nibl8872@a11-14.local>
2019-01-26 02:21:01 +0100

    can place objects on planet

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject tree, house;
+
+    private Camera mainCam;
+
+    public enum Placeable { Tree, House };
+    public Placeable selected = Placeable.Tree;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlaceObject();
+        }
+    }
+
+    public void SetSelected(string select)
+    {
+        selected = (Placeable)System.Enum.Parse(typeof(Placeable), select);
+    }
+
+    private void PlaceObject()
+    {
+        RaycastHit hit;
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(mainCam.transform.position, hit.point, Color.red);
+            Debug.DrawRay(mainCam.transform.position, mainCam.transform.position - hit.point, Color.green);
+            var startRot = Quaternion.LookRotation(hit.normal);
+
+            switch (selected)
+            {
+                case Placeable.Tree:
+                    {
+                        Instantiate(tree, hit.point, startRot);
+                        break;
+                    }
+
+                case Placeable.House:
+                    {
+                        Instantiate(house, hit.point, startRot);
+                        break;
+                    }
+                default:
+                    {
+                        Debug.Log("Error: No Placeable selected!");
+                        break;
+                    }
+            }
+        }
+
+
+    }
+}
